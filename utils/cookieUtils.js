51f587004a11cb0a9836c4c781569642b12f4b96
maykprogramador <133047863+maykprogramador@users.noreply.github.com>
2025-08-20 10:27:04 -0500

    Delete utils directory

--- utils/cookieUtils.js
+++
@@ -1,9 +0,0 @@
-// utils/cookieUtils.js
-export function setAuthCookie(res, token) {
-  res.cookie('access_token', token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'None',
-    maxAge: 1000 * 60 * 60, // 1 hora
-  });
-}
