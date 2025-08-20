2882bb43aa07c5dbee04f7fe8fe0d6ee4e466708
maykprogramador <133047863+maykprogramador@users.noreply.github.com>
2025-08-20 10:26:28 -0500

    Delete models directory

--- models/index.js
+++
@@ -1,7 +0,0 @@
-import './User.js'
-import './Bot.js'
-import './UsuarioBot.js'
-import './Registro.js'
-import './SolicitudUsuario.js'
-import './asociations.js'
-console.log('Models imported successfully');
